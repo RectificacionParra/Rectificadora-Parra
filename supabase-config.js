d9c5a47c416cd70618bf9a790019045a3183fc3b
Rectificacion Parra <thiagoferreyraa009@gmail.com>
2026-04-12 03:07:53 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.__SUPABASE_CONFIG = {
-  url: "https://project-y0y2d.vercel.app/",
+  url: "https://vimuezlncoxoiaizjkkh.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpbXVlemxuY294b2lhaXpqa2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NTA4MjUsImV4cCI6MjA5MTUyNjgyNX0.JwtcxWB2JVD6x6z1AIkT3FY-06UVI9OLUq7rn-eT4fY",
 };
