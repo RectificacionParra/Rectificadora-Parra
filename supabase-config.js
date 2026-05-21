b2df54f5a6fe2307239b2fb4889da839bb26494b
Rectificacion Parra <thiagoferreyraa009@gmail.com>
2026-05-21 09:20:41 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.__SUPABASE_CONFIG = {
-  url: "https://vimuezlncoxoiaizjkkh.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpbXVlemxuY294b2lhaXpqa2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NTA4MjUsImV4cCI6MjA5MTUyNjgyNX0.JwtcxWB2JVD6x6z1AIkT3FY-06UVI9OLUq7rn-eT4fY",
-};
+ // Configuración definitiva de conexión a la nube de Supabase para Rectificación Parra
+window.SUPABASE_URL = "https://vimuezlncoxoiaizjkkh.supabase.co";
+window.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpbXVlemxuY294b2lhaXpqa2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NTA4MjUsImV4cCI6MjA5MTUyNjgyNX0.JwtcxWB2JVD6x6z1AIkT3FY-06UVI9OLUq7rn-eT4fY";
